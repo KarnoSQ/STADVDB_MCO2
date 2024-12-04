bf88cf0ef72aa4d92f506460b1299d5b00bc03a9
Gabriele De Guzman <gabr.degu@gmail.com>
2024-12-04 14:14:43 +0800

    Improved first 3 test cases

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
@@ -8,32 +7,31 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// MySQL Connections to Nodes
-const node1 = mysql.createPool({
+var node1 = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'password123',
+  password: 'G@be060506',
   database: 'games'
 });
 
-const node2 = mysql.createPool({
+var node2 = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'password123',
-  database: 'window_game'
+  password: 'G@be060506',
+  database: 'games2'
 });
 
-const node3 = mysql.createPool({
+var node3 = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'password123',
-  database: 'otheros_game'
+  password: 'G@be060506',
+  database: 'games3'
 });
 
 app.get('/case1', async (req, res) => {
   try {
-    const [node1Data] = await node1.query('SELECT p.publisher FROM Games_Publishers p JOIN Games_Details d ON p.publisherID = d.publisherID WHERE p.publisher != "N/A" GROUP BY p.publisherID HAVING COUNT(d.appID) > 5;');
-    const [node2Data] = await node2.query('SELECT p.publisher FROM Games_Publishers p JOIN Games_Details d ON p.publisherID = d.publisherID WHERE p.publisher != "N/A" GROUP BY p.publisherID HAVING COUNT(d.appID) > 5;');
+    const [node1Data] = await node1.query('SELECT * FROM Games_Details WHERE appID = 20200');
+    const [node2Data] = await node2.query('SELECT * FROM Games_Details WHERE appID = 20200');
 
     res.json({
       case: 'Concurrent Reads',
@@ -46,23 +44,32 @@ app.get('/case1', async (req, res) => {
 });
 
 app.post('/case2', async (req, res) => {
-  const { newValue } = "New Title";
-
   try {
     const connection = await node1.getConnection();
     await connection.beginTransaction();
 
-    await connection.query('UPDATE Games_Details SET name = "New Title" WHERE appID = 20200', [newValue]);
+    await connection.query('UPDATE Games_Details SET price = 30 WHERE appID = 20200');
+    await connection.commit();
 
     const [readResult] = await node1.query('SELECT * FROM Games_Details WHERE appID = 20200');
+    
 
-    await connection.commit();
     connection.release();
+
+    const connection2 = await node2.getConnection();
+    await connection2.beginTransaction();
+
+    await connection2.query('UPDATE Games_Details SET price = 50.00 WHERE appID = 20200');
+    await connection2.commit();
+    
+    const [node2Data] = await node2.query('SELECT * FROM Games_Details WHERE appID = 20200');
+    connection2.release();
 
     res.json({
       case: 'Read and Write',
       write: 'Write completed',
-      read: readResult
+      node1: readResult,
+      node2: node2Data
     });
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -81,8 +88,8 @@ app.post('/case3', async (req, res) => {
     await connection2.beginTransaction();
 
  
-    await connection1.query('UPDATE Games_Details SET price = 20.00 WHERE appID = 20200', [value1]);
-    await connection2.query('UPDATE Games_Details SET price = 20.00 WHERE appID = 20200', [value1]);
+    await connection1.query('UPDATE Games_Details SET price = 20.00 WHERE appID = 20200');
+    await connection2.query('UPDATE Games_Details SET price = 20.00 WHERE appID = 20200');
 
     await connection1.commit();
     await connection2.commit();
