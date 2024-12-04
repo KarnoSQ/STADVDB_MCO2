1e7a490075e60726d30c4a541260eac93709067a
KarnoSQ <carlo_Sanbuenaventura@dlsu.edu.ph>
2024-12-04 15:46:00 +0800

    fixed some issues in recovery startegy

--- index.js
+++ index.js
@@ -136,32 +136,32 @@ app.post("/rcase1", async (req, res) => {
     const connection2 = await node2.getConnection();
 
     await connection.beginTransaction();
-    await connection2.beginTransaction();
-
-    const isNode1Available = await checkNode(connection);
+    
+    const isNode1Available = await checkNode(node1);
     if (!isNode1Available) {
-      await connection.commit();
 
+      await connection2.beginTransaction();
       await connection2.query(query, params);
       await connection2.commit();
 
-      connection.release();
-      connection2.release();
       WAL.push({ node: 'node1', query, params });
       return res.json({ message: "Node 1 is unavailable. Press Recovery When available" });
     }
+    
     await connection.query(query, params);
     await connection.commit();
 
+    await connection2.beginTransaction();
     await connection2.query(query, params);
     await connection2.commit();
     
-    connection.release();
-    connection2.release();
 
     res.json({case: 'Master Node transaction failure ', message: "Transaction completed" });
   } catch (err) {
     res.status(500).json({ error: err.message });
+  } finally{
+    if (connection) connection.release();
+    if (connection2)connection2.release();
   }
 });
 
@@ -173,27 +173,31 @@ app.post('/rcase2', async (req, res) => {
     const connection = await node1.getConnection();
     const connection2 = await node2.getConnection();
 
-    await connection.beginTransaction();
+    
     await connection2.beginTransaction();
 
-    await connection.query(query, params); 
-
     const isNode2Available = await checkNode(node2);
-    
     if (!isNode2Available) {
+      await connection.beginTransaction();
+      await connection.query(query, params); 
+      await connection.commit();
+
       WAL.push({ node: 'node2', query, params });
+      return res.json({ message: 'Node 2 is unavailable. Press Recovery When available' });
     } else {
-      await connection2.query(query, params); 
+      await connection2.query(query, params);
+      await connection2.commit(); 
     }
+    await connection.beginTransaction();
+    await connection.query(query, params); 
     await connection.commit();
-    await connection2.commit();
-
-    connection.release();
-    connection2.release();
-
-    res.json({ case: 'Slave Node Unavailable', message: 'Node 2 is unavailable.' });
+    res.json({ case: 'Slave Node Unavailable', message: 'Transaction Completed.' });
   } catch (err) {
     res.status(500).json({ error: 'Transaction failed mid-execution.' });
+  }
+  finally{
+    if (connection) connection.release();
+    if (connection2)connection2.release();
   }
 });
 
@@ -205,28 +209,28 @@ app.post('/rcase3', async (req, res) => {
     const connection = await node1.getConnection();
     const connection2 = await node2.getConnection();
 
-    await connection.beginTransaction();
+    
     await connection2.beginTransaction();
-
     await connection2.query(query, params);
     await connection2.commit();
 
+    await connection.beginTransaction();
     const isNode1Available = await checkNode(node1);
-    if (!isNode1Available) {
-      await connection.commit();
-      connection.release();
-      connection2.release();
+    if (!isNode1Available) {    
       WAL.push({ node: 'node1', query, params });
-      return res.json({ message: 'Node 1 is unavailable. Press Recovery When available' });
+      return res.json({ case: 'Master Node Write Failure', message: 'Node 1 is unavailable. Press Recovery When available' });
     }
-
-    await connection.query(query, params);
-    await connection.commit();
-    connection.release();
-    connection2.release();
+    else{
+      await connection.query(query, params);
+      await connection.commit();
+    }
     res.json({ case: 'Master Node Write Failure', message: 'Transaction completed.' });
   } catch (err) {
     res.status(500).json({ error: 'Node failed during transaction.' });
+  }
+  finally{
+    if (connection) connection.release();
+    if (connection2)connection2.release();
   }
 });
 
@@ -235,37 +239,28 @@ app.post('/rcase4', async (req, res) => {
   const params = [40.00, 20200];
 
   try {
-    const connection = await centralNode.getConnection();
-    const connection2 = await centralNode.getConnection();
+    const connection = await node1.getConnection();
+    const connection2 = await node2.getConnection();
     await connection.beginTransaction();
-    await connection2.beginTransaction();
     await connection.query(query, params); 
-
-    const isNode2Available = await checkNode(node2);
-
-    if (!isNode2Available) {
-      await connection2.query(query, params); 
-
-      await connection.commit();
-      connection.release();
-
-      await connection2.commit();
-      connection2.release();
-      WAL.push({ node: 'node2', query, params });
-      return res.json({ message: 'Node 2 is unavailable. Press Recovery When available' });
-    } else {
-      await node2.query(query, params);
-    }
-
-
     await connection.commit();
-    connection.release();
-    await connection2.commit();
-    connection2.release();
 
+    await connection2.beginTransaction();
+    const isNode2Available = await checkNode(node2);
+    if (!isNode2Available) {
+      WAL.push({ node: 'node2', query, params });
+      return res.json({ case: 'Slave Nodes Write Failure', message: 'Node 2 is unavailable. Press Recovery When available' });
+    } else {
+      await connection2.query(query, params);       
+      await connection2.commit();
+    }
     res.json({ case: 'Slave Nodes Write Failure', message: 'Transaction partially replicated.' });
   } catch (err) {
     res.status(500).json({ error: 'Failure during slave node writes.' });
+  }
+  finally{
+    if (connection) connection.release();
+    if (connection2)connection2.release();
   }
 });
 
@@ -273,14 +268,12 @@ app.post('/recover', async (req, res) => {
   try {
     for (const txn of WAL) {
       const pool = txn.node === 'node1' ? node1 : txn.node === 'node2' ? node2 : node3;
-
       try {
         await retryTransaction(pool, txn.query, txn.params);
       } catch (err) {
-        console.error(`Failed to recover transaction`);
+        console.error(`Failed to recover transaction for ${txn.node}: ${err.message}`);
       }
     }
-
     WAL.length = 0; // Clear the WAL after successful recovery
     res.json({ message: 'Recovery completed for all logged transactions.' });
   } catch (err) {
